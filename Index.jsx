2d56374f2c06baacd532094f71a83afcf9bdbb6c
ddoniddoni <psdkei@naver.com>
2020-06-23 19:09:48 +0900

    Test Drumkit

--- Index.jsx
+++ Index.jsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import Drumkit from "./Drumkit";
+import "./drumkit.css";
 
 export const Index = () => {
+  const playSound = (e) => {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+    key.classList.add("playing");
+    audio.currentTime = 0;
+    audio.play();
+  };
+
+  const removeTransition = (e) => {
+    if (e.propertyName !== "transform") return;
+    e.target.classList.remove("playing");
+  };
+
+  const keys = Array.from(document.querySelectorAll(".key"));
+  keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+  window.addEventListener("keydown", playSound);
   return (
     <>
-      <div>zz</div>
+      <Drumkit />
     </>
   );
 };
