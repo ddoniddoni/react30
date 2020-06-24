e6a54e6a3be0e6f7a119314eb061f1d92462635c
ddoniddoni <psdkei@naver.com>
2020-06-24 17:41:15 +0900

    Update Drumkit

--- Drumkit.jsx
+++ Drumkit.jsx
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./drumkit.css";
 
 export const Drumkit = (data) => {
-  console.log(data);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const playSound = (e) => {
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    console.log("어려워..", key);
+  };
+
+  useEffect(() => {
+    window.addEventListener("keydown", playSound);
+  }, []);
+
   return (
     <>
-      <div data-key={data.datakey} className={data.className}>
+      <div data-key={data.datakey} className="key">
         <kbd>{data.kbd}</kbd>
         <span className="sound">{data.sound}</span>
       </div>
-
-      <audio data-key={data.datakey} src="sounds/clap.wav"></audio>
+      <audio data-key={data.datakey} src={data.src}></audio>
     </>
   );
 };
--- Index.jsx
+++ Index.jsx
@@ -2,24 +2,29 @@ import React, { useState } from "react";
 import Drumkit from "./Drumkit";
 import "./drumkit.css";
 
-export const Index = () => {
-  const [datakey, setDatakey] = useState();
-  const [className, setClassName] = useState();
-  const [kbd, setKbd] = useState();
-  const [sound, setSound] = useState();
+import clap from "./sounds/clap.wav";
+import hihat from "./sounds/hihat.wav";
+import kick from "./sounds/kick.wav";
+import openhat from "./sounds/openhat.wav";
+import boom from "./sounds/boom.wav";
+import ride from "./sounds/ride.wav";
+import snare from "./sounds/snare.wav";
+import tom from "./sounds/tom.wav";
+import tink from "./sounds/tink.wav";
 
+export const Index = () => {
   return (
     <>
       <div className="keys">
-        <Drumkit data-key={65} className={"key"} kbd={"A"} sound={"clap"} />
-        <Drumkit data-key={83} className={"key"} kbd={"S"} sound={"hihat"} />
-        <Drumkit data-key={68} className={"key"} kbd={"D"} sound={"kick"} />
-        <Drumkit data-key={70} className={"key"} kbd={"F"} sound={"openhat"} />
-        <Drumkit data-key={71} className={"key"} kbd={"G"} sound={"boom"} />
-        <Drumkit data-key={72} className={"key"} kbd={"H"} sound={"ride"} />
-        <Drumkit data-key={74} className={"key"} kbd={"J"} sound={"snare"} />
-        <Drumkit data-key={75} className={"key"} kbd={"K"} sound={"tom"} />
-        <Drumkit data-key={76} className={"key"} kbd={"L"} sound={"tink"} />
+        <Drumkit data-key="65" kbd={"A"} sound={"clap"} src={clap} />
+        <Drumkit data-key="83" kbd={"S"} sound={"hihat"} src={hihat} />
+        <Drumkit data-key="68" kbd={"D"} sound={"kick"} src={kick} />
+        <Drumkit data-key="70" kbd={"F"} sound={"openhat"} src={openhat} />
+        <Drumkit data-key="71" kbd={"G"} sound={"boom"} src={boom} />
+        <Drumkit data-key="72" kbd={"H"} sound={"ride"} src={ride} />
+        <Drumkit data-key="74" kbd={"J"} sound={"snare"} src={snare} />
+        <Drumkit data-key="75" kbd={"K"} sound={"tom"} src={tom} />
+        <Drumkit data-key="76" kbd={"L"} sound={"tink"} src={tink} />
       </div>
     </>
   );
