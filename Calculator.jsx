c160b05d6ceb9fd4f61dee44d8c20be9c27cda43
ddoniddoni <psdkei@naver.com>
2020-06-27 04:21:37 +0900

    Update Calculator and Add Eval

--- Calculator.jsx
+++ Calculator.jsx
@@ -1,49 +1,79 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 export const Calculator = () => {
   const [number, setNumber] = useState("");
-  const [answer, setAnswer] = useState("");
+  const [result, setResult] = useState("");
   const [value, setValue] = useState("");
+  const inputEl = useRef(null);
 
-  const onClick = (e) => {};
+  const onClick = (e) => {
+    setValue(value + e.target.value);
+  };
+
+  const calResult = () => {
+    setResult(eval(value));
+  };
+
+  const numReset = () => {
+    setResult("");
+    setValue("");
+  };
   return (
     <>
       <div className="wrapper">
         <div>
           <form name="form">
-            <input className="input" type="text" />
+            <input
+              className="input"
+              value={value}
+              onChange={onClick}
+              ref={inputEl}
+            ></input>
           </form>
         </div>
         <div>
-          <input className="button" type="button" value="7" />
-          <input className="button" type="button" value="8" />
-          <input className="button" type="button" value="9" />
-          <input className="button" type="button" value="*" />
+          <p>{result}</p>
+        </div>
+        <div>
+          <input onClick={onClick} className="button" type="button" value="7" />
+          <input onClick={onClick} className="button" type="button" value="8" />
+          <input onClick={onClick} className="button" type="button" value="9" />
+          <input onClick={onClick} className="button" type="button" value="*" />
         </div>
 
         <div>
-          <input className="button" type="button" value="4" />
-          <input className="button" type="button" value="5" />
-          <input className="button" type="button" value="6" />
-          <input className="button" type="button" value="+" />
+          <input onClick={onClick} className="button" type="button" value="4" />
+          <input onClick={onClick} className="button" type="button" value="5" />
+          <input onClick={onClick} className="button" type="button" value="6" />
+          <input onClick={onClick} className="button" type="button" value="+" />
         </div>
 
         <div>
-          <input className="button" type="button" value="1" />
-          <input className="button" type="button" value="2" />
-          <input className="button" type="button" value="3" />
-          <input className="button" type="button" value="-" />
+          <input onClick={onClick} className="button" type="button" value="1" />
+          <input onClick={onClick} className="button" type="button" value="2" />
+          <input onClick={onClick} className="button" type="button" value="3" />
+          <input onClick={onClick} className="button" type="button" value="-" />
         </div>
 
         <div>
-          <input className="button" type="button" value="0" />
-          <input className="button" type="button" value="." />
-          <input className="button" type="button" value="/" />
-          <input className="button" type="button" value="=" />
+          <input onClick={onClick} className="button" type="button" value="0" />
+          <input onClick={onClick} className="button" type="button" value="." />
+          <input onClick={onClick} className="button" type="button" value="/" />
+          <input
+            onClick={calResult}
+            className="button"
+            type="button"
+            value="="
+          />
         </div>
 
         <div>
-          <input className="clear" type="button" value="Clear" />
+          <input
+            onClick={numReset}
+            className="clear"
+            type="button"
+            value="Clear"
+          />
         </div>
       </div>
     </>
