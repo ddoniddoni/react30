581d6adf637c6e1847b3e80cfafccfb2f1df6419
ddoniddoni <psdkei@naver.com>
2020-06-28 02:23:40 +0900

    Update key, keycode, code

--- KeyCode.jsx
+++ KeyCode.jsx
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from "react";
 import "./styles.css";
-import { KeyCodeDetail } from "./KeyCodeDetail";
 
 export const KeyCode = () => {
-  const [value, setValue] = useState("");
+  const [name, setName] = useState("");
+  const [code, setCode] = useState("");
+  const [eventCode, setEventCode] = useState("");
 
   const pressKeydown = (e) => {
     console.log(e);
+    setName(e.key);
+    setCode(e.keyCode);
+    setEventCode(e.code);
   };
 
   useEffect(() => {
@@ -18,6 +22,11 @@ export const KeyCode = () => {
       <div className="wrapper">
         <div className="wrapper__text">
           <p>자바스크립트 개발을 위한 키보드 Keycode</p>
+        </div>
+        <div className="wrapper__keycode">
+          <div className="keycode__name">{name}</div>
+          <div className="keycode__code">{code}</div>
+          <div className="keycode__eventCode">{eventCode}</div>
         </div>
       </div>
     </>
