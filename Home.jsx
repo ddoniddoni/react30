96f30ca8d5c57eaa43152acc0b1cf2205f8783a6
ddoniddoni <psdkei@naver.com>
2020-06-27 17:50:56 +0900

    Setting KeyCode

--- Home.jsx
+++ Home.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "./styles.css";
 const Home = () => {
-  return <div>안녕하세요~!</div>;
+  return (
+    <>
+      <div>안녕하세요~!</div>
+    </>
+  );
 };
 export default Home;
---
+++ KeyCode.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const KeyCode = () => {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+};
