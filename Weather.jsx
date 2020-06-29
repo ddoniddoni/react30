f9f086c0afa3897c59b2044604c89ce12856c1dd
ddoniddoni <psdkei@naver.com>
2020-06-30 00:25:48 +0900

    Update axios

--- Weather.jsx
+++ Weather.jsx
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./styles.css";
+import axios from "axios";
 
 export const Weather = () => {
-  return <div>날씨테스트</div>;
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then(({ data }) => setPosts(data));
+  }, []);
+
+  return (
+    <>
+      {posts.map((post, index) => (
+        <div key={index}>
+          <div>{post.title}</div>
+          <p>반갑습니다</p>
+          <div>{post.body}</div>
+        </div>
+      ))}
+    </>
+  );
 };
