b19de751f4000232d92fe889b3e68cd4b415d42b
ddoniddoni <psdkei@naver.com>
2020-06-29 17:03:04 +0900

    Keycode with css

--- KeyCode.jsx
+++ KeyCode.jsx
@@ -25,9 +25,21 @@ export const KeyCode = () => {
           <p>자바스크립트 개발을 위한 키보드 Keycode</p>
         </div>
         <div className="wrapper__keycode">
-          <div className="keycode__name">{name}</div>
-          <div className="keycode__code">{code}</div>
-          <div className="keycode__eventCode">{eventCode}</div>
+          <div className="keycode__bigcode">{code}</div>
+        </div>
+        <div className="wrapper__all">
+          <div className="wrapper__name">
+            <div className="keycode__ename">event.key</div>
+            <div className="keycode__name">{name}</div>
+          </div>
+          <div className="wrapper__which">
+            <div className="keycode__ewhich">event.which</div>
+            <div className="keycode__which">{code}</div>
+          </div>
+          <div className="wrapper__ecode">
+            <div className="keycode__ecode">event.code</div>
+            <div className="keycode__eventCode">{eventCode}</div>
+          </div>
         </div>
       </div>
     </>
