b6e5aba545dcfe025270560014251a415a4253ff
ddoniddoni <psdkei@naver.com>
2020-07-01 01:49:53 +0900

    Add hooks geolocation

--- Weather.jsx
+++ Weather.jsx
@@ -1,17 +1,15 @@
 import React, { useState, useEffect } from "react";
 import "./styles.css";
 import axios from "axios";
+import useGeolocation from "react-hook-geolocation";
+
 const API_KEY = "d8395d193557f6d8cb03d38b69442b17";
 export const Weather = () => {
   const [temps, setTemps] = useState([]);
-  const koreaPlace = {
-    Seoul: {
-      lat: 37.56667,
-      log: 126.97806,
-    },
-  };
+  const geolocation = useGeolocation();
 
   useEffect(() => {
+    console.log(geolocation.latitude);
     axios
       .get(
         `https://api.openweathermap.org/data/2.5/weather?lat=${37.56667}&lon=${126.97806}&appid=${API_KEY}&units=metric`
@@ -21,7 +19,7 @@ export const Weather = () => {
   console.log(temps);
   return (
     <>
-      <div>테스트중입니다ㅣ.</div>
+      <div>{geolocation.latitude}</div>
     </>
   );
 };
