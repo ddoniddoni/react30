152c59f374593c9c201d825820c2723a08e175a9
ddoniddoni <psdkei@naver.com>
2020-06-30 19:59:34 +0900

    Weather test

--- Weather.jsx
+++ Weather.jsx
@@ -1,25 +1,27 @@
 import React, { useState, useEffect } from "react";
 import "./styles.css";
 import axios from "axios";
-
+const API_KEY = "d8395d193557f6d8cb03d38b69442b17";
 export const Weather = () => {
-  const [posts, setPosts] = useState([]);
+  const [temps, setTemps] = useState([]);
+  const koreaPlace = {
+    Seoul: {
+      lat: 37.56667,
+      log: 126.97806,
+    },
+  };
 
   useEffect(() => {
     axios
-      .get("https://jsonplaceholder.typicode.com/posts")
-      .then(({ data }) => setPosts(data));
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${37.56667}&lon=${126.97806}&appid=${API_KEY}&units=metric`
+      )
+      .then(({ data }) => setTemps(data));
   }, []);
-
+  console.log(temps);
   return (
     <>
-      {posts.map((post, index) => (
-        <div key={index}>
-          <div>{post.title}</div>
-          <p>반갑습니다</p>
-          <div>{post.body}</div>
-        </div>
-      ))}
+      <div>테스트중입니다ㅣ.</div>
     </>
   );
 };
