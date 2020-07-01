b4421be2b64f83298722c3dbe5e177289e744425
ddoniddoni <psdkei@naver.com>
2020-07-01 16:58:31 +0900

    Update useAsync

--- User.jsx
+++ User.jsx
@@ -1,52 +1,15 @@
 import React, { useState, useEffect, useReducer } from "react";
 import "./styles.css";
 import axios from "axios";
+import { useAsync } from "./useAsync";
 
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "LOADING":
-      return {
-        loading: true,
-        data: null,
-        error: null,
-      };
-    case "SUCCESS":
-      return {
-        loading: false,
-        data: action.data,
-        error: null,
-      };
-    case "ERROR":
-      return {
-        loading: false,
-        data: null,
-        error: action.error,
-      };
-    default:
-      throw new Error(`Unhandled action type : ${action.type}`);
-  }
+const getUsers = async () => {
+  const response = await axios.get("https://jsonplaceholder.typicode.com/users");
+  return response.data;
 };
 
 export const User = () => {
-  const [state, dispatch] = useReducer(reducer, {
-    loading: false,
-    data: null,
-    error: null,
-  });
-
-  const fetchUsers = async () => {
-    dispatch({ type: "LOADING" });
-    try {
-      const response = await axios.get(`https://jsonplaceholder.typicode.com/users`);
-      dispatch({ type: "SUCCESS", data: response.data });
-    } catch (e) {
-      dispatch({ type: "ERROR", error: e });
-    }
-  };
-
-  useEffect(() => {
-    fetchUsers();
-  }, []);
+  const [state, refetch] = useAsync(getUsers, []);
 
   const { loading, data: users, error } = state;
 
@@ -62,7 +25,7 @@ export const User = () => {
           </li>
         ))}
       </ul>
-      <button onClick={fetchUsers}>다시 불러오기</button>
+      <button onClick={refetch}>다시 불러오기</button>
     </>
   );
 };
