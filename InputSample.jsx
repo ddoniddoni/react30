f6d323d913eb5be666b67485395b6eb899ebcb3c
ddoniddoni <psdkei@naver.com>
2020-07-03 17:44:36 +0900

    Add Array

--- InputSample.jsx
+++ InputSample.jsx
@@ -1,22 +1,38 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 export const InputSample = () => {
-  const [text, setText] = useState("");
+  const [inputs, setInputs] = useState({
+    name: "",
+    nickname: "",
+  });
+  const nameInput = useRef();
+
+  const { name, nickname } = inputs; // 비구조화할당
 
   const onChange = (e) => {
-    setText(e.target.value);
+    const { value, name } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
   };
 
   const onReset = () => {
-    setText("");
+    setInputs({
+      name: "",
+      nickname: "",
+    });
+    nameInput.current.focus();
   };
   return (
     <>
       <div>
-        <input onChange={onChange} value={text} />
+        <input name="name" placeholder="이름" onChange={onChange} value={name} ref={nameInput} />
+        <input name="nickname" placeholder="닉네임" onChange={onChange} value={nickname} />
         <button onClick={onReset}>초기화</button>
         <div>
-          <b>값 : {text} </b>
+          <b>값 : </b>
+          {name} ({nickname})
         </div>
       </div>
     </>
