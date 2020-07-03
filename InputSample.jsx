7c9f7cdd50130e961169fa9de58a1fd41efb4723
ddoniddoni <psdkei@naver.com>
2020-07-03 15:56:56 +0900

    Update InputSample

--- InputSample.jsx
+++ InputSample.jsx
@@ -1,5 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export const InputSample = () => {
-  return <div>반갑습니다!</div>;
+  const [text, setText] = useState("");
+
+  const onChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const onReset = () => {
+    setText("");
+  };
+  return (
+    <>
+      <div>
+        <input onChange={onChange} value={text} />
+        <button onClick={onReset}>초기화</button>
+        <div>
+          <b>값 : {text} </b>
+        </div>
+      </div>
+    </>
+  );
 };
