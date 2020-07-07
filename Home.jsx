e4731ad4137bab14eff68388370e9f69ae75cf90
ddoniddoni <psdkei@naver.com>
2020-07-08 03:48:33 +0900

    Setting TodoList

--- Home.jsx
+++ Home.jsx
@@ -1,39 +1,25 @@
-import React, { useEffect, useState } from "react";
-import List from "./List";
+import React from "react";
+import { createGlobalStyle } from "styled-components";
+import { TodoTemplate } from "./TodoTemplate";
+import { TodoHead } from "./TodoHead";
+import { TodoList } from "./TodoList";
+import { TodoCreate } from "./TodoCreate";
 
-import "./List.css";
+const GlobalStyle = createGlobalStyle`
+  body {
+    background:#e9ecef
+  }
+`;
 
 const Home = () => {
-  const [todos, setTodos] = useState(["js공부"]);
-  const [newTodo, setNewTodo] = useState();
-
-  const changeInputData = (e) => {
-    setNewTodo(e.target.value);
-  };
-
-  const addTodo = (e) => {
-    e.preventDefault();
-    setTodos([...todos, newTodo]);
-  };
-
-  useEffect(() => {
-    console.log("새로운 내용이 렌더링됐네요", todos);
-  }, [todos]);
-
   return (
     <>
-      <h1>Todo App</h1>
-
-      <form action="">
-        <input
-          className="input"
-          type="text"
-          name=""
-          onChange={changeInputData}
-        />
-        <button onClick={addTodo}>할일추가</button>
-      </form>
-      <List todos={todos} />
+      <GlobalStyle />
+      <TodoTemplate>
+        <TodoHead />
+        <TodoList />
+        <TodoCreate />
+      </TodoTemplate>
     </>
   );
 };
